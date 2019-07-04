3aad1bed3433da68eb2e3113b28a27522a32ee1b
Navid Jafarof <navidjafarof@gmail.com>
2019-07-04 18:30:19 +0430

    top entity!

---
+++ bmp_to_jpeg.vhdl
@@ -0,0 +1,15 @@
+library IEEE; library WORK;
+use work.jpeg_Package.all;
+
+entity bmp_to_jpeg is port (in_mem: in pixel_data_type
+                             width, height: in integer);
+end bmp_to_jpeg;
+architecture behavioral of bmp_to_jpeg is
+
+begin
+    read_row : for i in height-1 downto 0 loop
+        read_col : for j in 0 to width-1 loop
+            in_memdata(j)(i)
+        end loop read_col;
+    end loop read_row;
+end behavioral ;
